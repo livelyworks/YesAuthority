4389c1e51fc7a40f5c8ee31a26fac37cd095386b
Vinod Raut <yesteamtech@gmail.com>
2018-06-11 20:46:50 +0530

    Added YesAuthorityModelTrait with ifPermitted method to retrive only allowed items from database

---
+++ src/YesAuthorityModelTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace LivelyWorks\YesAuthority;
+/*
+ * Model Trait for YesAuthority
+ *-------------------------------------------------------- */
+
+trait YesAuthorityModelTrait
+{
+    /**
+     * Get the permitted items
+     * Please note:: this method uses JSON Functions which is available in  MySQL 5.7.8 & above
+     *
+     * @param string $query       - existing query
+     * @param string $permission  - permission to check
+     * @param string $table       - table name
+     * @param string $column  - permission to check
+     *
+     * @return query
+     *
+     * @since  11 JUN 2018
+     *------------------------------------------------------------------------ */
+    public function scopeIfPermitted($query, string $permission, string $table = '', string $column = '')
+    {
+        $tableName = '';
+        
+        if($table) {
+            if(!$column) {
+                throw new Exception('userColumn is required for table');
+            }
+
+            if(is_numeric($table) or is_numeric($column)) {
+                throw new Exception('Argument must be of the type string');
+            }
+
+           $query->join(
+                $table, $this->table.'.'.$this->primaryKey, 
+                '=', 
+                $table.'.'.$column
+            );
+
+           $tableName = $table . '.';
+        }
+
+        $query->whereRaw(
+            'JSON_CONTAINS('.$tableName.'__permissions->"$.allow", \'["'.$permission.'"]\') AND !JSON_CONTAINS('.$tableName.'__permissions->"$.deny", \'["'.$permission.'"]\')'
+        );
+
+        return $query;
+    }
+}
